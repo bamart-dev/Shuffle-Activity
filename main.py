95b8028ffc7b9de09aca0452041798645eb602ad
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-04-01 19:31:34 -0700

    added docstring to shuffle_deck()

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 # Implement a solution for shuffling a deck of cards
 # Use only randint() and no other imports
 def shuffle_deck(deck_of_cards):
+    """
+    input: deck_of_cards (list)
+    output: shuffled_deck (list)
+
+    Takes a list (deck_of_cards) argument and creates a new list
+    (shuffled_deck) with the elements of the old list shuffled randomly.
+    Returns the new shuffled_deck list.
+    """
     deck_size = len(deck_of_cards)
     shuffled_deck = []
     already_shuffled = []
